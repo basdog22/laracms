9585fd9a27402a2d9a0f7dbbedca773e14054b63
basdog22 <basdog22@gmail.com>
2014-07-21 16:29:30 +0300

    fixes

---
+++ app/controllers/UsersController.php
@@ -0,0 +1,40 @@
+<?php
+
+class UsersController extends BaseController {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Home Controller
+    |--------------------------------------------------------------------------
+    |
+    | You may wish to use controllers instead of, or in addition to, Closure
+    | based routes. That's great! Here is an example controller method to
+    | get you started. To route to this controller, just add the route:
+    |
+    |	Route::get('/', 'HomeController@showWelcome');
+    |
+    */
+
+    protected $layout = 'layouts.common.locked';
+
+    public function getLogin(){
+        $this->layout->content = View::make('backend/login');
+    }
+
+    public function getLogout(){
+        Auth::logout();
+        return Redirect::to('users/login')->with('message', 'Your are now logged out!');
+    }
+    public function postSignin() {
+        if (Auth::attempt(array('email'=>Input::get('email'), 'password'=>Input::get('pass')),true)) {
+            return Redirect::intended('/backend/');
+        } else {
+            return Redirect::to('users/login')
+                ->with('message', 'Your username/password combination was incorrect')
+                ->withInput();
+        }
+    }
+
+
+
+}
