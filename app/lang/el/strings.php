1c592b27650270905d31151b8df5409b01ff65c8
basdog22 <basdog22@gmail.com>
2014-07-21 17:41:11 +0300

    fixes

---
+++ app/lang/el/strings.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'login_please'  =>  'Παρακαλώ συνδεθείτε',
+    'login'         =>  'Σύνδεση',
+    'email_address' =>  'Διεύθυνση Email',
+    'password'      =>  'Κωδικός Πρόσβασης'
+);
