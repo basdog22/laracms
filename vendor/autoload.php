3a0da8d759d6d4460fade8c482994511dc837144
basdog22 <basdog22@gmail.com>
2014-07-21 17:42:45 +0300

    fixes

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit43630b5a1796a3ae05070f5bb25b0c38::getLoader();
+return ComposerAutoloaderInit7164fefa57b070bbffeb144e65bdb6e3::getLoader();
